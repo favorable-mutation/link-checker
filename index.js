1e337222a141c8b54554147756df6acf2e134ffe
Griffin Rademacher <contact@griffin-rademacher.info>
2021-01-25 15:57:07 -0500

    takes an initial pass at setting up redis

--- index.js
+++ index.js
@@ -1,29 +1,42 @@
+const Redis = require("ioredis");
+const redis = new Redis(process.env.REDIS_URL);
 const linkinator = require("linkinator");
 const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 5000;
+const statusKey = "checkStatus";
+const urlsKey = "checkUrls";
 
 express()
   .use(express.static(path.join(__dirname, "public")))
   .set("views", path.join(__dirname, "views"))
   .set("view engine", "ejs")
-  .get("/", (req, res) => res.render("pages/index"))
-  .get("/check", (req, res) =>
+  .get("/", (req, res) =>
+    redis
+      .get(statusKey)
+      .then((status) => {
+        redis
+          .lrange(urlsKey, 0, -1)
+          .then((errorUrls) => res.render("pages/index", { status, errorUrls }))
+          .catch((error) => res.send(`error: ${error}`));
+      })
+      .catch((error) => res.send(`error: ${error}`))
+  )
+  .get("/check", (req, res) => {
+    res.sendStatus(200);
+    redis.del(statusKey);
+    redis.del(urlsKey);
+    redis.set(statusKey, "RUNNING");
     linkinator
       .check({
         path: "https://pages.github.ccs.neu.edu/jhemann/21SP-CS4400",
         recurse: true,
       })
-      // TODO persist status instead of getting it every time
-      //        with what datastore? write to a file?
-      // TODO display full linkinator results
       .then(({ passed, links }) => {
-        res.send({
-          status: passed,
-          errorUrls: links
-            .filter(({ state }) => state === "BROKEN")
-            .map(({ url }) => url),
-        });
-      })
-  )
+        redis.set(statusKey, passed ? "SUCCESS" : "FAILURE");
+        links
+          .filter(({ state }) => state === "BROKEN")
+          .forEach(({ url }) => redis.lpush(urlsKey, url));
+      });
+  })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));

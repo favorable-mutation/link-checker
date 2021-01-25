042b22ace3b253fe5155e1329ff89dce45b5a42d
Griffin Rademacher <contact@griffin-rademacher.info>
2021-01-25 12:06:07 -0500

    Adds initial call to linkinator

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const linkinator = require("linkinator");
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .use(express.static(path.join(__dirname, "public")))
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "ejs")
+  .get("/", (req, res) => res.render("pages/index"))
+  .get("/linkinator", (req, res) => {
+    linkinator
+      .check({
+        path: "https://pages.github.ccs.neu.edu/jhemann/21SP-CS4400",
+        recurse: true,
+      })
+      .then(({ passed }) => res.send(passed));
+  })
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
